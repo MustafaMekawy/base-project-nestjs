62c555998ac6d054f294d1584a1ff5e8338e7e26
mustafa mekawy <manomostafa29@gmail.com>
2023-06-11 17:17:32 +0300

    multer

---
+++ src/factory/storgehelper.ts
@@ -0,0 +1,15 @@
+import { diskStorage } from "multer";
+
+export class MyMulter {
+    static Storage = (dest:string) => {
+        return diskStorage({
+                      destination: dest,
+                      filename: (req, file, cb) => {
+                        const ext = file.originalname.split(".").pop()
+                        const newName = Date.now()+"."+ext
+                        cb(null, newName);
+                      },   
+                    })          
+                }
+  }
+
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Res, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MyMulter } from 'src/factory/storgehelper';
 
+export const storage = {
+  storage: MyMulter.Storage('./uploads/profileimages')
+};
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -26,4 +33,18 @@ export class UserController {
   remove(@Param('id') id: string, @Res() res) {
     return this.userService.remove(id,res);
   }
+  @UseGuards(JwtGuard)
+  @Get('myprofile')
+    getprofile(@GetUser() user:CreateUserDto,@Res() res){
+      delete user.resetExpiresTime
+      delete user.resetPasswordToken
+      return this.userService.getProfile(user,res)
+    }
+
+    @Post('uploadImage')
+    @UseInterceptors(FileInterceptor('image', storage))
+    uploadFile(@UploadedFile() file, @GetUser('id') userId:any,@Res()res) {
+
+      return this.userService.updatePhoto(userId, file,res);
+    }
 }
