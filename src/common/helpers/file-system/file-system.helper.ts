9c836246f3429b4d5d4cc6f10b9218f128fb8722
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2023-06-30 01:31:21 +0300

    file system helper

---
+++ src/common/helpers/file-system/file-system.helper.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import fs from 'fs';
+import util from 'util';
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const unlinkFile = util.promisify(fs.unlink);
+export class FileSystemHelper {
+  static getFilePath(relativePath: string) {
+    return path.join(__dirname, relativePath);
+  }
+  static createDir(dirPath: string) {
+    try {
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  static async removeFile(relativePath: string) {
+    await unlinkFile(this.getFilePath(relativePath));
+  }
+  static async readFile(relativePath: string, options: any = {}) {
+    const filePath = this.getFilePath(relativePath);
+    return await readFile(filePath, options);
+  }
+  static async writeFile(
+    relativePath: string,
+    data: string | NodeJS.ArrayBufferView,
+    options: any = {},
+  ) {
+    const filePath = this.getFilePath(relativePath);
+    return await writeFile(filePath, data, options);
+  }
+}
